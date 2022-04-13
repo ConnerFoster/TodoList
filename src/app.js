5ae615234cd8bb5242b02b6fa5b539fccb77d0d2
Conner Foster <connerf99@gmail.com>
2022-04-13 19:43:06 -0400

    Parcel setup, Task class created, project class created

---
+++ src/app.js
@@ -0,0 +1,5 @@
+import Task from "./task";
+
+//let thisTask = new Task("Clean", new Date(2022, 11, 23));
+
+//console.log(thisTask);
---
+++ src/projects.js
@@ -0,0 +1,12 @@
+class Project {
+  constructor(name) {
+    this.name = name;
+    this.tasks = [];
+  }
+  get name() {
+    return this.name;
+  }
+  set name(name) {
+    this.name = name;
+  }
+}
